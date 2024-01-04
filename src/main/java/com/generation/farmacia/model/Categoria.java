d1881e889706bd2ed7e78383763b852b971df2f6
jenifferribeiro <js.ribei@hotmail.com>
2024-01-04 11:39:17 -0300

    Criação CRUD-Produto

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -17,6 +24,10 @@ public class Categoria {
 	
 	@NotNull(message = "O Atributo tipo é obrigatório")
 	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -33,6 +44,13 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
-	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+		
 }
